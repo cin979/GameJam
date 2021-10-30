46c71b5e6fec2fac173858e526656f4b2a0e972e
Connor <cin979@gmail.com>
2021-10-30 14:07:42 +1000

    Modified Colours

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   playerSprite = createSprite(windowWidth/2, windowHeight/2);
   playerSprite.draw = function() {
-    stroke("BLUE");
+    stroke("BLACK");
     strokeWeight(5);
     noFill();
     ellipse(0, 0, 20, 20);
@@ -10,6 +10,6 @@ function setup() {
 }
 
 function draw() {
-  background("ORANGE");
+  background("YELLOW");
   drawSprites();
 };
