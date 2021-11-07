cdd774563613c7cff5a9ea38970638a7128c998d
PhuocThienTran <donnyquickinc@gmail.com>
2021-11-07 23:04:46 +1000

    test

--- p5/game/sketch.js
+++ p5/game/sketch.js
@@ -1,10 +1,7 @@
 //GLOBAL VARS
 let track1;
 let track2;
-<<<<<<< HEAD
-=======
 let track3;
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
 let grass;
 let road;
 let key;
@@ -16,19 +13,12 @@ let crashSound;
 let mode;
 let timer = 5;
 
-<<<<<<< HEAD
-const W = 1440; const H = 700;
-=======
 // const W = windowWidth; const H = windowHeight;
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
 
 function preload() {
     track1 = loadStrings("track/track1.txt");
     track2 = loadStrings("track/track2.txt");
-<<<<<<< HEAD
-=======
     track3 = loadStrings("track/track3.txt");
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
 
     grass = loadImage("images/rock.png");
     road = loadImage("images/path.png");
@@ -36,25 +26,13 @@ function preload() {
     carImg = loadImage("images/main.png");  
     accident = new Group(); //if car = grass
     normal = new Group();
-<<<<<<< HEAD
-    level = new Group()
-=======
     difficult2 = new Group(); //for level 2
     difficult3 = new Group(); // for level 3
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
 }
 
 //MAP TILES
 let level1; 
 function setup() {
-<<<<<<< HEAD
-    createCanvas(W,H);
-
-    mode = 0;
-
-    level1 = new Array(track1.length);
-    for (i = 0; i < track1.length; i++) {
-=======
     createCanvas(windowWidth,windowHeight);
 
     mode = 0;
@@ -62,7 +40,6 @@ function setup() {
     // Make level1 first
     level1 = new Array(track1.length);
     for (i = 0; i < track1.length && track1[i][0] != ""; i++) {
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
         level1[i] = track1[i].split(""); //split token for txt values
     }
     for(y = 0; y < level1.length; y++) { //tile's y in accordance to txt file
@@ -71,36 +48,14 @@ function setup() {
         }
     }
 
-<<<<<<< HEAD
-    car = createSprite(carX,carY);
-=======
     car = createSprite(30+carX,30+carY);
     car.rotation = 270;
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
     car.addImage(carImg);
     car.scale = carScale;
 }
 
 //CREATE SPRITES 
 function makeSprites(value,x,y) { //makeSprite params: value,x,y from for loop
-<<<<<<< HEAD
-    sprite = createSprite(x*50,y*50);
-    if(value == 0){ //value based on txt
-        sprite.addImage(grass);
-        sprite.addToGroup(accident)
-        sprite.setCollider("circle",0,0,5,5);
-        sprite.scale = (0.80)
-    }
-    if(value == 1){
-        sprite.addImage(road);
-        sprite.addToGroup(normal);
-    }
-    if(value == 2){
-        sprite.addImage(key);
-        sprite.addToGroup(level);
-        sprite.setCollider("circle",0,0,5,5);
-        sprite.scale = (0.80)
-=======
     sprite = createSprite(30+x*50,30+y*50, 50, 50);
     if(value == 0){ // grass
         // sprite.scale = (0.80);
@@ -134,20 +89,14 @@ function makeSprites(value,x,y) { //makeSprite params: value,x,y from for loop
         sprite.addImage(key);
         sprite.addToGroup(difficult3);
         // sprite.setCollider("rectangle",0,0,5,5);
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
     }
 }
 
 let level2;
 function level2Track(){
-<<<<<<< HEAD
-    level2 = new Array(track2.length);
-    for (i = 0; i < track2.length; i++) {
-=======
     // create level 2 track
     level2 = new Array(track2.length);
     for (i = 0; i < track2.length && track2[i][0] != ""; i++) {
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
         level2[i] = track2[i].split(""); //split token for txt values
     }
     for(y = 0; y < level2.length; y++) { //tile's y in accordance to txt file
@@ -160,44 +109,6 @@ function level2Track(){
     car.scale = carScale;
 }
 
-<<<<<<< HEAD
-//CAR VARS:
-let carScale = 0.4
-let carX = W/2; //car initial x W/2 
-let carY = 625; //car initial y 625
-let SPEED = 0; //initial speed
-
-function carDrive() {
-    car.setSpeed(SPEED);
-    if(keyCode == UP_ARROW) {
-        SPEED+=0.01;
-        car.maxSPEED = 1;
-    }
-    else if(keyCode == DOWN_ARROW) {
-        SPEED -=0.01;
-        if(SPEED = 0) {
-            SPEED = 0;
-        }
-    }
-    else if(keyCode == LEFT_ARROW) {
-        car.rotation-=1;
-
-    }
-    else if(keyCode == RIGHT_ARROW) {
-        car.rotation+=1;
-    }
-
-    if(car.collide(accident)) {
-        SPEED = 0;
-        car.position.x = carX;
-        car.position.y = carY;
-    }
-
-    if(car.collide(level)) {
-        level2Track();
-
-    }
-=======
 let level3;
 function level3Track(){
     // create level 3 track
@@ -266,7 +177,6 @@ function carDrive() {
         level3Track();
     }
     
->>>>>>> 88ca40ec9f933e9cc6f68658b3a9aed33320f5b5
 }
 
 function instruction() {
@@ -296,4 +206,3 @@ function draw() {
     }
 
 }
-
